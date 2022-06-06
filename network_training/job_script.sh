712e19e8205479e98c1fab44bd399a35db7fe837
hmray2025 <70339195+hmray2025@users.noreply.github.com>
2022-06-06 15:59:53 +0200

    Updates to testing script

--- network_training/job_script.sh
+++ network_training/job_script.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hura1154
 #SBATCH --time=00:02:00
-#SBATCH --partition=alpine
+#SBATCH --partition=shas
 #SBATCH --job-name=vae-trainer
 #SBATCH --output=vae-trainer-%j.out
 
