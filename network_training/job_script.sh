dadc418450921c012c90d8ade3f35a7863059c1a
hmray2025 <70339195+hmray2025@users.noreply.github.com>
2022-06-06 15:19:18 +0200

    Updated CURC testing script

--- network_training/job_script.sh
+++ network_training/job_script.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hura1154
-#SBATCH --time=00:05:00
+#SBATCH --time=00:02:00
 #SBATCH --partition=shas-testing
 #SBATCH --job-name=vae-trainer
 #SBATCH --output=vae-trainer-%j.out
@@ -14,4 +14,4 @@ module purge
 module load julia/1.6.0
 module load cuda/11.2
 
-./projects/hura1154/preference_elicitation/network_training/vae_train.jl
+julia ./projects/hura1154/preference_elicitation/Interaction.jl
