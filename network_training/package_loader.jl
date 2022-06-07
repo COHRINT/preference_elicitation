27e2d669dde74fd3f77c1ee95b17762c03de76a7
hmray2025 <70339195+hmray2025@users.noreply.github.com>
2022-06-07 12:49:07 +0200

    New packages to package loader

--- network_training/package_loader.jl
+++ network_training/package_loader.jl
@@ -1,2 +1,2 @@
 using Pkg
-Pkg.add(["LaTeXStrings","ImageIO","CategoricalArrays","CUDA","Flux","BSON","DrWatson","Images","Logging","Parameters","ProgressMeter","TensorBoardLogger","Random","MosaicViews","BasicPOMCP","POMDPs","ParticleFilters","Distributions","LinearAlgebra","Distances","Statistics","POMDPPolicies"])
+Pkg.add(["LaTeXStrings","ImageIO","CategoricalArrays","CUDA","Flux","BSON","DrWatson","Images","Logging","Parameters","ProgressMeter","TensorBoardLogger","Random","MosaicViews","BasicPOMCP","POMDPs","ParticleFilters","Distributions","LinearAlgebra","Distances","Statistics","POMDPPolicies","POMDPModelTools","MAT","CSV"])
