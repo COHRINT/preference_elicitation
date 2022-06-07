021698c749a46de9739a851a1be3d31d02facb4d
hmray2025 <70339195+hmray2025@users.noreply.github.com>
2022-06-07 12:38:16 +0200

    Changes to loading script

--- network_training/job_script.sh
+++ network_training/job_script.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hura1154
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --partition=shas
 #SBATCH --job-name=vae-trainer
 #SBATCH --output=vae-trainer-%j.out
@@ -14,6 +14,6 @@ module purge
 module load julia/1.6.0
 
 echo
-julia ./projects/hura1154/preference_elicitation/network_training/package_loader.jl
-julia ./projects/hura1154/preference_elicitation/Interaction.jl
+julia ./preference_elicitation/network_training/package_loader.jl
+julia ./preference_elicitation/Interaction.jl
 echo
