3e7dddbeced653d2db61478fb63bc3738b80cc61
hmray2025 <70339195+hmray2025@users.noreply.github.com>
2022-06-07 11:47:30 +0200

    New package loading for RC

--- network_training/job_script.sh
+++ network_training/job_script.sh
@@ -12,13 +12,8 @@
 
 module purge
 module load julia/1.6.0
-module load cuda/11.2
 
 echo
-julia
-using Pkg
-Pkg.add(["ImageIO","CategoricalArrays","CUDA","Flux","BSON","DrWatson","Images","Logging","Parameters","ProgressMeter","TensorBoardLogger","Random","MosaicViews","BasicPOMCP","POMDPs","ParticleFilters","Distributions","LinearAlgebra","Distances","Statistics","POMDPPolicies"])
-exit()
-pwd
+julia ./projects/hura1154/preference_elicitation/network_training/package_loader.jl
 julia ./projects/hura1154/preference_elicitation/Interaction.jl
 echo
