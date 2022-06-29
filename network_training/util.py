04b8a19fd21d510b03cfde0612a11f92927df56c
hmray2025 <70339195+hmray2025@users.noreply.github.com>
2022-06-29 09:46:18 -0600

    Working PyTorch VAE implementation

--- network_training/util.py
+++ network_training/util.py
@@ -1,7 +1,8 @@
 import pickle as pk
 import sys
-
-
+import random
+from matplotlib import image
+import numpy as np
 
 
 ############################################################
@@ -11,16 +12,17 @@ def disp_to_term(msg):
     sys.stdout.write(msg + '\r')
     sys.stdout.flush()
 
+
 def load_pickle(filename):
     try:
         p = open(filename, 'r')
     except IOError:
-        print "Pickle file cannot be opened."
+        print("Pickle file cannot be opened.")
         return None
     try:
         picklelicious = pk.load(p)
     except ValueError:
-        print 'load_pickle failed once, trying again'
+        print('load_pickle failed once, trying again')
         p.close()
         p = open(filename, 'r')
         picklelicious = pk.load(p)
@@ -28,7 +30,25 @@ def load_pickle(filename):
     p.close()
     return picklelicious
 
+
 def save_pickle(data_object, filename):
     pickle_file = open(filename, 'w')
     pk.dump(data_object, pickle_file)
     pickle_file.close()
+
+
+def sample_image(filename, save_path, set_name, n_samples, sample_size):
+    """Randomly samples and saves images to a given directory."""
+    data = image.imread(filename)
+    x_avail = data.shape[0] - sample_size - 1
+    y_avail = data.shape[1] - sample_size - 1
+    x_set = range(0, x_avail)
+    y_set = range(0, y_avail)
+
+    for s in range(0, n_samples+1):
+        x_sample = random.choice(x_set)
+        y_sample = random.choice(y_set)
+        new_img = data[x_sample:x_sample + sample_size, y_sample:y_sample + sample_size]
+        path = str(save_path + set_name + str(s) + ".jpeg")
+        # print(path)
+        image.imsave(path, new_img)
