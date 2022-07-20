7bf18a6d5db22642f64e6083d87a73d3b234cd65
Hunter Ray <hura1154@login13.rc.colorado.edu>
2022-07-20 14:50:31 -0600

    Added saved model comments

--- network_training/python_job.sh
+++ network_training/python_job.sh
@@ -5,13 +5,13 @@
 #SBATCH --partition=sgpu
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hura1154
-#SBATCH --time=8:00:00
+#SBATCH --time=6:00:00
 #SBATCH --job-name=py-vae-trainer
-#SBATCH --output=py-vae-trainer-%j.out
+#SBATCH --output=py-vae-trainer-6-%j.out
 
 
 module load anaconda
 module load cuda/10.2
 conda activate vae_python
-python network_training/vanila_vae.py --lr 1e-4 --beta 1
+python network_training/vanila_vae.py --nerve 32 --lvs 150 --beta 10 
 
